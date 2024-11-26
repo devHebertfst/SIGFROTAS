6db17aac7a2b16e312d3a91cacd4b33062482dcd
Hebert França <hebertfst@hotmail.com>
2024-11-26 20:50:26 -0300

    First Commit

---
+++ src/main/java/br/ufrn/imd/sigfrotas/SigfrotasApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.sigfrotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigfrotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigfrotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrn/imd/sigfrotas/SigfrotasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.sigfrotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigfrotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
